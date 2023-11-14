4874b239f95cb55b8cccc7b798aeb898618b1d24
LRFSousa <luisricardofs@gmail.com>
2023-11-14 10:03:15 -0300

    Atualização das entidades usando lombok

--- src/main/java/com/lrfs/minhasfinancas/model/entities/Lancamento.java
+++ src/main/java/com/lrfs/minhasfinancas/model/entities/Lancamento.java
@@ -5,24 +5,36 @@ import java.time.LocalDate;
 
 import org.springframework.data.convert.Jsr310Converters;
 
+import com.lrfs.minhasfinancas.model.enums.Status;
+import com.lrfs.minhasfinancas.model.enums.TipoLancamento;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
 
 @Entity
 @Table(name = "lancamento", schema = "financas")
+@Data
+@Builder
 public class Lancamento {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	public Long id;
+	
+	@Column
+	public String descricao;
 	
 	@Column
 	public Integer mes;
@@ -40,5 +52,13 @@ public class Lancamento {
 	@Column(name = "data_cadastro")
 	@Convert(converter = Jsr310Converters.DateToLocalDateConverter.class)
 	private LocalDate dataCadastro;
+
+	@Column
+	@Enumerated(value = EnumType.STRING)
+	private TipoLancamento tipo;
 	
+	@Column
+	@Enumerated(value = EnumType.STRING)
+	private Status status;
+
 }
